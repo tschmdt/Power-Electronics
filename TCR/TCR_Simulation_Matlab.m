12c6ca8ab0438a409bb29917b5ebaaaf6e89a32a
Timo <e12112219@student.tuwien.ac.at>
2024-08-29 13:12:55 +0200

    FINALISING TCR PLOTS

--- TCR/TCR_Simulation_Matlab.m
+++ TCR/TCR_Simulation_Matlab.m
@@ -1,12 +1,11 @@
-%% 1) Change a to change firing angle
-    % run first then run section 2
-
+%% Plot with 4 subplots
+    % Change a to change firing angle
+    % Best if plot @ 90°, 125°, 165°
+    
 open_system('TCR_Simulation')
-a = 125;
-out= sim('TCR_Simulation')
 
-%% 2) Plot with 4 subplots
-% Best if plot @ 90°, 125°, 165°
+a = 165;
+out= sim('TCR_Simulation')
 
 fontname = 'Helvetica';
 
@@ -34,25 +33,24 @@ set(gca, 'FontSize', 14);
 x = ((a + 360) / 360) * (1 / 50);
 
 if a == 90
-    plot([0.019998 0.019998], [0 390],'--', 'LineWidth', 1, 'Color',[0.800    0.800    0.800]); %fix line
-    plot([x x], [0 390], '--', 'LineWidth', 1, 'Color', [ 0.8000    0.8000    0.8000]); % variable line
-    x1 = [0.01998 x];
-    y1 = [350 350];
-    quiver(x1(1), y1(1), x1(2)-x1(1), y1(2)-y1(1), 0, 'LineWidth', 1, 'MaxHeadSize', 2,'ShowArrowHead','off','Color',[0.502 0.502 0.502], 'LineStyle','--');
+    plot([0.019998 0.019998], [0 390],'--', 'LineWidth', 1, 'Color', [0.6510    0.6510    0.6510]); %fix line
+    plot([x x], [0 390], '--', 'LineWidth', 1, 'Color', [0.6510    0.6510    0.6510]); % variable line
+    Start = [0.01998 350];
+    End = [x 350];
+    arrow(Start, End, 'Length', 5, 'Width', .5, 'Color', [0.6510    0.6510    0.6510]);
     text(x+0.001, 353, ['\alpha = ', num2str(a), '°'], 'HorizontalAlignment', 'left', 'FontSize', 14);
 elseif a > 90 && a < 165
-     plot([0.019998 0.019998], [0 230],'--', 'LineWidth', 1, 'Color',[0.800    0.800    0.800]); %fix line
-     plot([x x], [0 230], '--', 'LineWidth', 1,'Color', [ 0.8000    0.8000    0.8000]); % variable line
-     x1 = [0.01998 x];
-     y1 = [200 200];
-     quiver(x1(1), y1(1), x1(2)-x1(1), y1(2)-y1(1), 0, 'LineWidth', 1, 'MaxHeadSize', 2,'ShowArrowHead','off','Color',[0.502 0.502 0.502], 'LineStyle','--');
-     text(x+0.001, 203, ['\alpha > 90° (\alpha = ', num2str(a), '°)'], 'HorizontalAlignment', 'left', 'FontSize', 14);
+     plot([0.019998 0.019998], [0 230],'--', 'LineWidth', 1, 'Color', [0.6510    0.6510    0.6510]); %fix line
+     plot([x x], [0 230], '--', 'LineWidth', 1,'Color', [0.6510    0.6510    0.6510]); % variable line
+     Start = [0.01998 200];
+     End = [x 200];
+    arrow(Start, End, 'Length', 5, 'Width', .5, 'Color', [0.6510    0.6510    0.6510]);     text(x+0.001, 203, ['\alpha > 90° (\alpha = ', num2str(a), '°)'], 'HorizontalAlignment', 'left', 'FontSize', 14);
 elseif a >= 165 && a <= 180
-    plot([0.019998 0.019998], [0 140],'--', 'LineWidth', 1, 'Color',[0.800    0.800    0.800]); %fix line
-    plot([x x], [0 140], '--', 'LineWidth', 1, 'Color', [ 0.8000    0.8000    0.8000]); % variable line
-    x1 = [0.01998 x];
-    y1 = [130 130];
-    quiver(x1(1), y1(1), x1(2)-x1(1), y1(2)-y1(1), 0, 'LineWidth', 1, 'MaxHeadSize', 2,'ShowArrowHead','off','Color',[0.502 0.502 0.502], 'LineStyle','--');
+    plot([0.019998 0.019998], [0 140],'--', 'LineWidth', 1, 'Color', [0.6510    0.6510    0.6510]); %fix line
+    plot([x x], [0 140], '--', 'LineWidth', 1, 'Color', [0.6510    0.6510    0.6510]); % variable line
+    Start = [0.01998 130];
+    End = [x 130];
+    arrow(Start, End, 'Length', 5, 'Width', .5, 'Color', [0.6510    0.6510    0.6510]);
     text(x+0.001, 133, ['\alpha ~ 180° (\alpha = ', num2str(a), '°)'], 'HorizontalAlignment', 'left', 'FontSize', 14);
 end
 
@@ -67,14 +65,13 @@ hold off;
 
 %% Plot with 3 subplots
 
+open_system('TCR_Simulation') 
+
 fontname = 'Helvetica';
 
-t = out.Load_Voltage.time;
-U_a = out.Load_Voltage.Data;
-I_a = out.I_TCR.Data;
-titles = {'\alpha = 90°', '\alpha > 90°', '\alpha ~ 180°'};
-
 alpha = [90, 125, 155];
+
+titles = {'\alpha = 90°', '\alpha > 90°', '\alpha ~ 180°'};
 
 figure;
 
@@ -89,7 +86,7 @@ for i = 1: length(alpha)
     %plot(t, U_a, t, I_a, 'LineWidth', 2)
     h1 = plot(t, U_a, 'LineWidth', 2, 'Color', [0.6510    0.6510    0.6510]); 
     hold on;
-    h2 = plot(t, I_a, 'LineWidth', 2.5, 'Color',[0.8500 0.3250 0.0980]); 
+    h2 = plot(t, I_a, 'LineWidth', 2.5, 'Color', [0.8500 0.3250 0.0980]); 
 
     ax = gca;
     ax.XAxisLocation = 'origin';
@@ -109,11 +106,11 @@ for i = 1: length(alpha)
     arrow(Start, End, 'Length', 5, 'Width', .5, 'Color', [0.502 0.502 0.502])
    
     plot([0.019998 0.019998], [0 220],'--', 'LineWidth', 1, 'Color',[0.502 0.502 0.502]); %fix line
-    plot([x x], [0 220], '--', 'LineWidth', 1, 'Color',[0.502 0.502 0.502]); % variable line
+    plot([x x], [0 220], '--', 'LineWidth', 1, 'Color', [0.502 0.502 0.502]); % variable line
 
    
     if i == 1
-        legend([h1, h2], {'Voltage U_a', 'Current I_a'}, 'Location', 'best', 'FontSize', 10);
+        legend([h1, h2], {'Load Voltage U', 'TCR Current I'}, 'Location', 'best', 'FontSize', 10);
     end
        
 end
