d2c2629a2231ce5cc85f6853d6a5c7731c2285cc
Timo <e12112219@student.tuwien.ac.at>
2024-09-05 10:54:39 +0200

    TCSC

--- TCSC/TCSC_original_v2.m
+++ TCSC/TCSC_original_v2.m
@@ -1,5 +1,4 @@
-addpath('C:\Users\E370_\Documents\Arbeit\Git\Power-Electronics\TCSC');
+
 openExample('simscapeelectricalsps/ThyristorControlledSeriesCapacitorExample');
-cd
 cd C:\Users\E370_\Documents\Arbeit\Git\Power-Electronics\TCSC
 pwd
