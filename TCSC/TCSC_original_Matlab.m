19a347d1f9f251a438f6e1df6b4abeef4bdc24df
Timo <e12112219@student.tuwien.ac.at>
2024-09-19 09:58:19 +0200

    ADD TCSC plot ZP (one plot)
    save w and w/ annotation and small

--- TCSC/TCSC_original_Matlab.m
+++ TCSC/TCSC_original_Matlab.m
@@ -32,7 +32,7 @@ ylabel('deg');
 title('\alpha', 'FontSize', 14,'Interpreter', 'tex');
 set(gca, 'FontSize', 12);
 
-%% Power, Z, alpha one figure
+%% Power, Z, alpha ONE FIGURE
 fontname = 'Helvetica';
 
 figure('Position', [100, 100, 600, 800]);
@@ -62,38 +62,35 @@ xlabel('Zeit (s)');
 ylabel('deg');
 title('alpha', 'FontSize', 12, 'FontWeight', 'bold', 'Interpreter', 'tex');
 set(gca, 'FontSize', 12, 'FontName', fontname);
-ylim([65 95])
+ylim([65 95]);
 
-%%
-
-
-%% Power, Z, alpha one plot
+%% Power, Z, alpha ONE PLOT
 fontname = 'Helvetica';
 
-figure;
-plot(P, 'LineWidth', 2, 'Color', [0 0.4470 0.7410]);
-hold on;
-ylabel('MW');
-title('Power', 'FontSize', 12);
-set(gca, 'FontSize', 12, 'FontName', fontname);
-set(gca, 'XTick', []);
-xlabel('')
+%figure;
+figure('Position', [100, 100, 750, 700]);
 
-plot(Ztcsc, 'LineWidth', 2, 'Color', [0.8500 0.3250 0.0980]);
+yyaxis left
+plot(Ztcsc.Time, Ztcsc.Data, 'LineWidth', 2); 
 ylabel('Ohm');
-title('Z_{TCSC}', 'FontSize', 12, 'Interpreter', 'tex');
+set(gca, 'FontSize', 12);
+%set(gca, 'XTick', []); 
+xlabel('Zeit (s)'); 
+
+title('Wirkleistungsfluss-Änderung durch Impedanz-Regelung', 'FontSize', 16, 'FontWeight', 'bold');
+
+yyaxis right
+plot(P.Time, P.Data, 'LineWidth', 2, 'LineStyle','-.'); 
+ylabel('MW');  
 set(gca, 'FontSize', 12, 'FontName', fontname);
-set(gca, 'XTick', []);
-xlabel('');
-x = [0.3 0.218];  % arrow beginn 0.75/25; end 0.53/75; normalized
-y = [0.45 0.5];
-annotation('textarrow',x,y, 'String', ' start regulation to Z_{ref}= 128 Ohm', LineWidth= 1.1, FontSize=11)
- 
-plot(alpha, 'LineWidth', 2, 'Color', [0.9290 0.6940 0.1250]);
-xlabel('Zeit (s)');
-ylabel('deg');
-title('alpha', 'FontSize', 12, 'FontWeight', 'bold', 'Interpreter', 'tex');
-set(gca, 'FontSize', 12, 'FontName', fontname);
+set(gca, 'YTick', [0, 200, 400, 600, 800, 1000]);
+
+
+legend('Z_{TCSC}','P')
+
+x = [0.35 0.27]; 
+y = [0.25 0.3];
+annotation('textarrow', x, y, 'String', ' start regulation to Z_{ref} = 128 \Omega', 'LineWidth', 1.1, 'FontSize', 12);
 
 %% U,I 
     % capacitor voltage and current at steady state operation in the capacitive
